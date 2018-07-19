13c4f5832dd1837f6f47c6708c77bc692a89924a
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-19 19:30:12 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,13 @@
 class Author
+  @@posts = []
   attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
     @posts = []
   end
+
+  def add_post(post)
+    @posts << post
+    @@posts << post
 end
