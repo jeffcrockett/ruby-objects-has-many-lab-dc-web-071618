8ab2068a6527384cb354d97ed7c5774f9e5aea3d
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-19 19:47:39 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,15 @@
 require_relative 'author'
 
 class Post
-  attr_accessor :title, :author, :author_name
+  attr_accessor :title, :author
   @@all = []
 
   def initialize(title)
     @title = title
     @@all << self
+  end
+
+  def author_name 
+    self.author.name
   end
 end
